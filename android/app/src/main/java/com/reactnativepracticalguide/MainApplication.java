c68b958023226da091cc4e6fdec1c5f684baa398
Thakur Mahendar Singh <thakurmahendar.singh@accoliteindia.com>
2019-02-14 16:30:31 +0530

    added react navigation with linking

--- android/app/src/main/java/com/reactnativepracticalguide/MainApplication.java
+++ android/app/src/main/java/com/reactnativepracticalguide/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactnativepracticalguide;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -30,6 +31,7 @@ import java.util.List;
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
           new VectorIconsPackage()
       );
     }
